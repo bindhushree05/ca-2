a2c8d87d3690136df99c55d6aff4f6d176d7ec55
bindhushree05 <141924979+bindhushree05@users.noreply.github.com>
2023-11-25 10:39:24 +0530

    Add files via upload

---
+++ tom.js
@@ -0,0 +1,88 @@
+
+
+var rows = 3;
+var columns = 3;
+
+var currTile;
+var otherTile; 
+
+
+var Moves=0;
+
+var imgOrder = ["19", "18", "17", "16", "15", "14", "13", "12", "11"];
+
+window.onload = function() {
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < columns; c++) {
+
+            let tile = document.createElement("image");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+
+            tile.addEventListener("dragstart", dragStart);  
+            tile.addEventListener("dragover", dragOver);    
+            tile.addEventListener("dragenter", dragEnter);  
+            tile.addEventListener("dragleave", dragLeave);  
+            tile.addEventListener("drop", dragDrop);        
+            tile.addEventListener("dragend", dragEnd);      
+
+            document.getElementById("board").append(tile);
+
+        }
+    }
+}
+
+function dragStart() {
+    currTile = this; 
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; 
+}
+
+function dragEnd() {
+    if (!otherTile.src.includes("13.jpg")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-"); 
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = r == r2 && c2 == c-1;
+    let moveRight = r == r2 && c2 == c+1;
+
+    let moveUp = c == c2 && r2 == r-1;
+    let moveDown = c == c2 && r2 == r+1;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+
+        currTile.src = otherImage;
+        otherTile.src = currImage;
+
+        Moves += 1;
+        document.getElementById("Moves").innerText = Moves;
+    }
+
+
+}
